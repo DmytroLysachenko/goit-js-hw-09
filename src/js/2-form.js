24a527232aabce3319622479f385190b19833af8
Dimka863 <dlysachenko98@gmail.com>
2024-04-03 11:16:37 +0200

    Upd for better code

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,9 +1,7 @@
 const form = document.querySelector('.feedback-form');
 const textArea = form.elements.message;
 const email = form.elements.email;
-
-const extractedValues = JSON.parse(localStorage.getItem('feedback-form-state'));
-const savedValues = { ...extractedValues };
+const savedValues = JSON.parse(localStorage.getItem('feedback-form-state'));
 const submittedMsgs = [];
 
 let msgBox = document.createElement('p');
@@ -16,14 +14,15 @@ form.prepend(msgBox);
 email.value = savedValues.email ?? '';
 textArea.value = savedValues.message ?? '';
 
-form.addEventListener('input', event => {
-  event.target.type === 'email'
-    ? (savedValues.email = event.target.value.trim())
-    : (savedValues.message = event.target.value.trim());
+const onFormInput = event => {
+  const { target: targetElement } = event;
+  const inputName = targetElement.name;
+  const inputValue = targetElement.value.trim();
+  savedValues[inputName] = inputValue;
   localStorage.setItem('feedback-form-state', JSON.stringify(savedValues));
-});
+};
 
-form.addEventListener('submit', event => {
+const onSubmit = event => {
   event.preventDefault();
   if (textArea.value !== '' && email.value !== '') {
     submittedMsgs.push(savedValues);
@@ -35,4 +34,8 @@ form.addEventListener('submit', event => {
   } else {
     msgBox.classList.remove('visually-hidden');
   }
-});
+};
+
+form.addEventListener('input', onFormInput);
+
+form.addEventListener('submit', onSubmit);
