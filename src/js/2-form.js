d3d429f7037dc36a930f88a99a910c1252783475
Dimka863 <dlysachenko98@gmail.com>
2024-04-03 11:57:07 +0200

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,20 +1,33 @@
 const form = document.querySelector('.feedback-form');
 const textArea = form.elements.message;
 const email = form.elements.email;
-const savedValues = JSON.parse(localStorage.getItem('feedback-form-state'));
 const submittedMsgs = [];
 
 let msgBox = document.createElement('p');
-
 msgBox.textContent = 'Wrong values, please type in Email and Message';
 msgBox.classList.add('msg-box');
 msgBox.classList.add('visually-hidden');
 form.prepend(msgBox);
 
-email.value = savedValues.email ?? '';
-textArea.value = savedValues.message ?? '';
+const initialLoad = () => {
+  try {
+    const exportedValues = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    if (exportedValues === null) {
+      return;
+    }
+    email.value = exportedValues.email ?? '';
+    textArea.value = exportedValues.message ?? '';
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+initialLoad();
 
 const onFormInput = event => {
+  const savedValues = JSON.parse(localStorage.getItem('feedback-form-state'));
   const { target: targetElement } = event;
   const inputName = targetElement.name;
   const inputValue = targetElement.value.trim();
@@ -22,20 +35,19 @@ const onFormInput = event => {
   localStorage.setItem('feedback-form-state', JSON.stringify(savedValues));
 };
 
+form.addEventListener('input', onFormInput);
+
 const onSubmit = event => {
   event.preventDefault();
   if (textArea.value !== '' && email.value !== '') {
     submittedMsgs.push(savedValues);
     msgBox.classList.add('visually-hidden');
-    email.value = '';
-    textArea.value = '';
     localStorage.removeItem('feedback-form-state');
+    form.reset();
     console.log(submittedMsgs);
   } else {
     msgBox.classList.remove('visually-hidden');
   }
 };
-
-form.addEventListener('input', onFormInput);
 
 form.addEventListener('submit', onSubmit);
